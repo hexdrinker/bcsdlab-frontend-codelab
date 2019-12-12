de055a7f63d87f3ad586da5e15e376d05de4612b
박영호 <mellonia.bluerose@gmail.com>
2019-12-12 14:35:18 +0900

    App 컴포넌트 수정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import TodoContainer from './components/TodoContainer';
+import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+import TodoInputForm from './components/TodoInputForm';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const unDoneTodos = todos.filter(todo => !todo.done).length;
+  const onCreate = text => {
+    if (text === '') {
+      alert("할 일을 입력해주세요");
+      return;
+    }
+    setTodos(todos.concat({
+      id: todos.length + 1,
+      text,
+      done: false
+    }))
+  }
+
+  const onToggle = id => {
+    setTodos(todos.map(todo => todo.id === id ? { ...todo, done: !todo.done } : todo));
+  }
+  
+  const onRemove = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoContainer>
+        <TodoHead unDoneTodos={unDoneTodos} />
+        <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove} />
+        <TodoInputForm onCreate={onCreate} />
+      </TodoContainer>
     </div>
   );
 }
